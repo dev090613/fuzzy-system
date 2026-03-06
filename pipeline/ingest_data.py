0f90659dd9874855ade8f7145bc3ec3509039819
kokoro <128305501+dev090613@users.noreply.github.com>
2026-03-06 03:25:34 +0000

    add docker-compose

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
@@ -10,14 +11,6 @@ month = 1
 prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/"
 file_name = f"yellow_tripdata_{year}-{month:02d}.csv.gz"
 url = prefix + file_name
-
-pg_user = "root"
-pg_pass = "root"
-pg_host = "localhost"
-pg_port = "5432"
-pg_db = "ny_taxi"
-
-chunksize = 100000
 
 dtype = {
     "VendorID": "Int64",
@@ -40,26 +33,39 @@ dtype = {
 
 parse_dates = ["tpep_pickup_datetime", "tpep_dropoff_datetime"]
 
-engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
 
-df_iter = pd.read_csv(
-    url, dtype=dtype, parse_dates=parse_dates, iterator=True, chunksize=chunksize
-)
+@click.command()
+@click.option("--pg-user", "pg_user", required=True, help="PostgreSQL username")
+@click.option("--pg-pass", "pg_pass", required=True, help="PostgreSQL password")
+@click.option("--pg-host", "pg_host", default="localhost", help="PostgreSQL host")
+@click.option("--pg-port", "pg_port", default="5432", help="PostgreSQL port")
+@click.option("--pg-db", "pg_db", required=True, help="PostgreSQL database name")
+@click.option("--target-table", "target_table", required=True, help="Target table name")
+def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
 
+    chunksize = 100000
+    engine = create_engine(
+        f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}"
+    )
 
-def run():
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize,
+    )
+
     first = True
     for df_chunk in tqdm(df_iter):
         if first:
             # Create table schema (no data)
-            df_chunk.head(0).to_sql(
-                name="yellow_taxi_data", con=engine, if_exists="replace"
-            )
+            df_chunk.head(0).to_sql(name=target_table, con=engine, if_exists="replace")
             first = False
             print("Table created")
 
         # Insert chunk
-        df_chunk.to_sql(name="yellow_taxi_data", con=engine, if_exists="append")
+        df_chunk.to_sql(name=target_table, con=engine, if_exists="append")
 
         print("Inserted:", len(df_chunk))
 
